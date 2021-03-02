a4aafa9543e8cd50bbf61d1841b4080c990cb901
Cleverson Sampaio <clevsampaio@outlook.com.br>
2021-03-02 08:29:59 -0300

    First commit

---
+++ src/test/java/br/com/dbserver/AppTest.java
@@ -0,0 +1,13 @@
+package br.com.dbserver;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue(true);
+    }
+}
